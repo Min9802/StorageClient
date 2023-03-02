bd83c5854ec2dc8f5852b4062b2da2d1678126f3
Min <Hieumin9802@gmail.com>
2023-03-02 17:42:57 +0700

    update
    update

--- src/Providers/StorageProvider.php
+++ src/Providers/StorageProvider.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
 use League\Flysystem\Filesystem;
 
-use Min\StorageClient\Commands\PublishConfig;
 use Min\StorageClient\StorageAdapter;
 use Min\StorageClient\StorageClient;
 use Min\StorageClient\StorageDrive;
@@ -17,14 +16,23 @@ class StorageProvider extends ServiceProvider
 {
     public function register()
     {
-
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/client.php',
+            'client'
+        );
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/storage.php',
+            'storage'
+        );
     }
     public function boot()
     {
-        // Register the "publish:config" command
-        $this->commands([
-            PublishConfig::class,
-        ]);
+         $this->publishes([
+            __DIR__.'/../config/client.php' => config_path('client.php'),
+        ], 'config');
+         $this->publishes([
+            __DIR__.'/../config/storage.php' => config_path('storage.php'),
+        ], 'config');
         try {
             Storage::extend('msv', function ($app, $config) {
                 $options = [];
@@ -44,19 +52,5 @@ class StorageProvider extends ServiceProvider
         } catch (\Exception$e) {
             Log::error('Message :' . $e->getMessage() . '--line: ' . $e->getLine());
         }
-    }
-    /**
-     * Publishes the configuration file for the package.
-     *
-     * @return void
-     */
-    public function publishConfig()
-    {
-        $this->publishes([
-            __DIR__.'/config/client.php' => config_path('client.php'),
-        ], 'config');
-         $this->publishes([
-            __DIR__.'/config/storage.php' => config_path('storage.php'),
-        ], 'config');
     }
 }
